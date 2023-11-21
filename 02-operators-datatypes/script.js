bafba226449dfd4307d642e9577f8cb27b6a03ee
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2023-11-21 10:42:32 +0100

    feat: tasks datatypes and operators

---
+++ 02-operators-datatypes/script.js
@@ -0,0 +1,27 @@
+console.log("J" + "e" + "n" + "n" + "y");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 == 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" != 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+console.log("" + 4);
+
+//Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 == 50 / 10);
+console.log(25 / 5 && 50 / 10 === 5);
