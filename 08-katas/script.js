8e4aa6205591857325c88d1d22bad53ae595ce12
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2023-12-05 19:58:08 +0100

    adds: more katas

--- 08-katas/script.js
+++ 08-katas/script.js
@@ -7,6 +7,37 @@ const stringToNumber = function (str) {
   return parseInt(str);
 };
 
+/*
+/**********************************************************/
+// CHECK SAME CASE //
+/**********************************************************/
+
+function sameCase(a, b){
+  function containsNonLetterChars(str) {
+    return /[^A-Za-z]/.test(str);
+  }
+
+  if (typeof a === "string" && typeof b === "string" && !containsNonLetterChars(a) && !containsNonLetterChars(b)) { 
+    const upperA = a.toUpperCase();
+    const upperB = b.toUpperCase();
+    const lowerA = a.toLowerCase();
+    const lowerB = b.toLowerCase();
+
+    if ((upperA === a && upperB === b) || (lowerA === a && lowerB === b)){
+      return 1;
+    } else if ((upperA === a && lowerB === b) || (lowerA === a && upperB === b) ||
+               (upperB === b && lowerA === a) || (lowerB === b && upperA === a)){
+      return 0;
+    } else {
+      return -1;
+    }
+  } else {
+    return -1;
+  }
+}
+
+
+
 /**********************************************************/
 // NARCISSISTIC NUMBER //
 /**********************************************************/
@@ -535,3 +566,271 @@ function highAndLow(numbers) {
 
   return `${highest} ${lowest}`; // Return the highest and lowest numbers separated by a space
 }
+/**********************************************************/
+//Who is the killer?//
+/**********************************************************/
+
+function killer(suspectInfo, dead) {
+  for (const suspectName in suspectInfo) {
+    let seenBySuspect = suspectInfo[suspectName];
+    let isKiller = true;
+    for (let i = 0; i < dead.length; i++) {
+      if (seenBySuspect.indexOf(dead[i]) === -1) {
+        isKiller = false;
+        break;
+      }
+    }
+    if (isKiller) {
+      return suspectName;
+    }
+  }
+}
+//console.log (killer({'Brad': [], 'Megan': ['Ben', 'Kevin'], 'Finn': []}, ['Ben']), 'Megan');
+
+/**********************************************************/
+//Who's Online?//
+/**********************************************************/
+
+const whosOnline = (friends) => {
+  const online = [];
+  const offline = [];
+  const away = [];
+
+  const output = {};
+
+  for (let friend of friends) {
+    if (friend.status === "online" && friend.lastActivity <= 10)
+      online.push(friend.username);
+    if (friend.status === "online" && friend.lastActivity > 10)
+      away.push(friend.username);
+    if (friend.status === "offline") offline.push(friend.username);
+  }
+
+  if (online.length !== 0) output.online = online;
+  if (offline.length !== 0) output.offline = offline;
+  if (away.length !== 0) output.away = away;
+
+  return output;
+};
+
+/**********************************************************/
+//Find the murderer//
+/**********************************************************/
+
+function sc(said, suspects) {
+  let matchingSuspects = []; //empty array to stuff match name in
+
+  for (let i = 0; i < suspects.length; i++) {
+    // iterate through suspects list
+    let suspect = suspects[i].toLowerCase(); //stuff lower case suspects here to compare
+    let nameMatches = true; // define a true condition to use later
+
+    for (let j = 0; j < said.length; j++) {
+      // iterate through the said word
+      if (
+        said[j].toLowerCase() !== "~" &&
+        said[j].toLowerCase() !== suspect[j]
+      ) {
+        //ignore the ~ && turn the true condition of nameMathes to false if letters are different
+        nameMatches = false;
+        break; // if letters are different, get out of the loop, continue with next word
+      }
+    }
+    if (nameMatches) {
+      // if name matches is still true until here
+      matchingSuspects.push(suspects[i]); // stuff the name into the matchingSuspects array
+    }
+  }
+
+  return matchingSuspects.join(","); //and convert it back into a string before returning it
+}
+
+/**********************************************************/
+//Convert number to reversed array of digits//
+/**********************************************************/
+
+function digitize(n) {
+  return n.toString().split('').reverse().map(Number);
+  }
+
+/**********************************************************/
+//So Many Permutations!//
+/**********************************************************/
+
+  function permutations(string) {
+    let results = [];
+  
+    if (string.length === 1) {
+      return [string];
+    }
+  
+    for (let i = 0; i < string.length; i++) {
+      let firstChar = string[i];
+      let charsLeft = string.substring(0, i) + string.substring(i + 1);
+      let innerPermutations = permutations(charsLeft);
+  
+      for (let j = 0; j < innerPermutations.length; j++) {
+        results.push(firstChar + innerPermutations[j]);
+      }
+    }
+  
+    return Array.from(new Set(results));
+  }
+
+/**********************************************************/
+//Find the position!//
+/**********************************************************/
+
+  Find the position!
+  function position(letter){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const position = alphabet.indexOf(letter)+1
+    return "Position of alphabet: " + position;
+    }
+
+
+/**********************************************************/
+//String incrementer//
+/**********************************************************/
+
+    function incrementString (strng) {
+      //get the chars//
+      let chars = strng.replace(/[0-9]*$/, ''); 
+      
+      //get the numbers//
+      let numPart = strng.slice(chars.length);
+    
+      // If there are no numbers, append 1 to the end of the string//
+      if (numPart === '') {
+        return strng + '1';
+      }
+    
+      // Increment the number part
+      let num = parseInt(numPart); // Convert the number part to an integer
+      num++; // Increment the number
+      let numStr = num.toString(); // Convert the incremented number back to a string
+    
+      // Pad the number part with leading zeros if needed
+      while (numStr.length < numPart.length) {
+        numStr = '0' + numStr;
+      }
+    
+      return chars + numStr; // Return the new string
+    }
+
+/***********************************************************************/
+//Looking for vowels in a number array and take them out into new array//
+/**********************************************************************/
+
+    function isVow(a){
+      const a2String = a.toString();
+      const vowels = a2String.match(/[aeiou]/g);
+        if (vowels != null){
+          return vowels;
+        }
+        else {
+        return a;
+        }
+      return vowels;
+    }
+
+    //const a = [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]
+
+
+/***********************************************************************/
+//Is there a vowel in there? (convert numbers into letters via ascii code)//
+/**********************************************************************/
+
+function isVow(a) {
+  const result = a.map(function(element){
+    if (typeof element === 'number' && element >= 97 && element <= 122) {
+      const char = String.fromCharCode(element);
+      if (/[aeiou]/.test(char)) {
+        return char;
+      }
+    }
+    return element;
+  });
+  return result;
+}
+
+/***********************************************************************/
+//Count of positives / sum of negatives//
+/**********************************************************************/
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0){
+    return [];
+  }
+  
+  let countPositives = 0;
+  let sumNegatives = 0;
+
+    for (let i = 0; i<input.length; i++){
+     if (input[i] > 0){
+       countPositives++;
+     }
+      else if (input[i]<=0){
+       sumNegatives += input[i];
+      }
+    }
+  let result = [countPositives, sumNegatives];
+  return result;
+}
+ //console.log(countPositivesSumNegatives([0, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+
+
+ /***********************************************************************/
+//Fake Binary//
+/**********************************************************************/
+
+ function fakeBin(x) {
+  const regex = /[0-4]/g; //g means global and will then iterate through all in x not only the first find
+  return x.toString().replace(regex, "0").replace(/[5-9]/g, "1");
+}
+
+
+ /***********************************************************************/
+//Sum of positive//
+/**********************************************************************/
+
+function positiveSum(arr) {
+  
+  if (arr.length === 0){
+    return 0;
+    }
+  
+  let sum = 0;
+  
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]>=0){
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+ /***********************************************************************/
+//Moving Zeros To The End//
+/**********************************************************************/
+
+function moveZeros(arr) {
+  let zerosCount = 0;
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      result.push(arr[i]);
+    } else {
+      zerosCount++;
+    }
+  }
+
+  for (let j = 0; j < zerosCount; j++) {
+    result.push(0);
+  }
+
+  return result;
+}
+
+//console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
