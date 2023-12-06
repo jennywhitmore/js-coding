99839af7b6a7cb5739ed0d67c091b904c379f100
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2023-12-06 15:41:31 +0100

    adds : more basic solution to filter api data kata

--- 09-coding-foundation-katas-main/filter-api-data/filter-api-data.js
+++ 09-coding-foundation-katas-main/filter-api-data/filter-api-data.js
@@ -1,3 +1,5 @@
+/******My approach after some travelling through google***************/
+
 function filterApiData(apiData, mandatoryKeys) {
   const filter_apiData = apiData.filter(function (object) {
     return mandatoryKeys.every(function (key) {
@@ -21,3 +23,44 @@ function filterApiData(apiData, mandatoryKeys) {
 -> https://www.javascripttutorial.net/javascript-every/
 -> a lot of typing, changing things and brute force testing if it works ;)
 */
+
+/******Here comes a more basic approach to solve this kata***************/
+
+function filterApiData(apiData, mandatoryKeys) {
+  // Create an empty array to store the filtered objects
+  let filteredData = [];
+
+  // Iterate through each object in the apiData array
+  for (let i = 0; i < apiData.length; i++) {
+    let obj = apiData[i];
+    let hasAllKeys = true;
+
+    // Iterate through each mandatory key
+    for (let j = 0; j < mandatoryKeys.length; j++) {
+      let key = mandatoryKeys[j];
+      // Check if the current object has the mandatory key
+      if (!obj.hasOwnProperty(key)) {
+        // If any mandatory key is missing, set hasAllKeys to false and break out of the loop
+        hasAllKeys = false;
+        break;
+      }
+    }
+
+    // If the object has all the mandatory keys, add it to the filteredData array
+    if (hasAllKeys) {
+      filteredData.push(obj);
+    }
+  }
+
+  // Return the array of filtered objects
+  return filteredData;
+}
+
+/*****************The Plan:***************/
+//1. Create an empty array to store the objects that meet the mandatory keys condition//
+//2. Use for-loop to iterate through each object in the `apiData` array.//
+//3. Inside the outer loop, use another for-loop to iterate through each mandatory key in the mandatoryKeys array, and for each key…//
+//4. …use the hasOwnProperty method to check if the current object contains that key.//
+//4. If not all mandatory key are found in the current object, set the hasAllKeys variable to false and break out of the inner loop.//
+//5. If the object has all the mandatory keys, add it to the filteredData array.//
+//6. Return the filteredData array containing the filtered objects.//
